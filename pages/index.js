13ff7536bf101790433bc1ed14bf968eb50a49c9
Elena Pavlova <lenlho22@gmail.com>
2020-10-04 18:03:04 +0300

    Add universal links

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
     <div className="main">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">
+              <a>Posts</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <h1>Hello Home Page</h1>
     </div>
   )
