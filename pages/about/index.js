8223663234ee8825c677f9cbfc4d409b8e446f1c
Elena Pavlova <lenlho22@gmail.com>
2020-10-04 18:15:12 +0300

    Add navigation by next router

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,15 @@
+import Router from 'next/router'
+
 const About = () => {
+  const handlerGoHome = () => {
+    Router.push('/')
+  }
+
   return (
     <div className="main">
       <h1>About Page</h1>
+      <button onClick={handlerGoHome}>Go back to Home</button>
+      <button onClick={() => Router.push('/posts')}>Go back to Posts</button>
     </div>
   )
 }
