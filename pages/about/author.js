06bd174a67cbc5e7e755672ac9bce437fd7dfad2
Elena Pavlova <lenlho22@gmail.com>
2020-10-04 17:50:19 +0300

    Create nested route for about and about author pages

---
+++ pages/about/author.js
@@ -0,0 +1,11 @@
+// route -> http://localhost:3000/about/author
+
+const AboutAuthor = () => {
+  return (
+    <div className="main">
+      <h1>About Author Page</h1>
+    </div>
+  )
+}
+
+export default AboutAuthor
