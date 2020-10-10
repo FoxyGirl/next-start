76e2d6f4da9cdc9d2bd0a020ee3dfd852bad3d80
Elena Pavlova <lenlho22@gmail.com>
2020-10-10 19:11:44 +0300

    Add json-server and mock data for posts

--- pages/posts.js
+++ pages/posts.js
@@ -1,10 +1,31 @@
+import { useState, useEffect } from 'react'
 import MainLayout from '../components/MainLayout'
 
+const baseUrl = 'http://localhost:4200'
+
 const Posts = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const load = async () => {
+      const response = await fetch(`${baseUrl}/posts`)
+      const loadedPosts = await response.json()
+
+      setPosts(loadedPosts)
+    }
+
+    load()
+  }, [])
+
   return (
     <MainLayout title="Posts">
       <div className="main">
         <h1>Posts Page</h1>
+        <ul>
+          {posts.map(({ id, title }) => (
+            <li key={id}>{title}</li>
+          ))}
+        </ul>
       </div>
     </MainLayout>
   )
