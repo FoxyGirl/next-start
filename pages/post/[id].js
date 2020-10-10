68bb3084974159edc270167fbc243050b2e58dd4
Elena Pavlova <lenlho22@gmail.com>
2020-10-10 20:13:31 +0300

    Add getInitialProps to get data for SSR

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,14 +1,34 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { baseUrl } from '../../constants'
+import MainLayout from '../../components/MainLayout'
+import styles from '../../styles/Posts.module.scss'
 
-const Post = () => {
+const Post = ({ post }) => {
   const router = useRouter()
-  console.log('router', router)
+
+  const { title, body } = post
 
   return (
-    <div className="main">
-      <h1>Post Page {router.query.id}</h1>
-    </div>
+    <MainLayout title={`Post: ${router.query.id}`}>
+      <div className="main">
+        <h1>{title}</h1>
+        <hr />
+        <p>{body}</p>
+        <hr />
+        <Link href="/posts">
+          <a className={styles.link}>Back to all posts</a>
+        </Link>
+      </div>
+    </MainLayout>
   )
+}
+
+Post.getInitialProps = async (ctx) => {
+  const { id } = ctx.query
+  const res = await fetch(`${baseUrl}/posts/${id}`)
+  const post = await res.json()
+  return { post }
 }
 
 export default Post
--- pages/posts.js
+++ pages/posts.js
@@ -1,34 +1,30 @@
-import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { baseUrl } from '../constants'
 import MainLayout from '../components/MainLayout'
 
-const baseUrl = 'http://localhost:4200'
-
-const Posts = () => {
-  const [posts, setPosts] = useState([])
-
-  useEffect(() => {
-    const load = async () => {
-      const response = await fetch(`${baseUrl}/posts`)
-      const loadedPosts = await response.json()
-
-      setPosts(loadedPosts)
-    }
-
-    load()
-  }, [])
-
+const Posts = ({ posts }) => {
   return (
     <MainLayout title="Posts">
       <div className="main">
         <h1>Posts Page</h1>
         <ul>
           {posts.map(({ id, title }) => (
-            <li key={id}>{title}</li>
+            <li key={id}>
+              <Link href="/post/[id]" as={`/post/${id}`}>
+                <a>{title}</a>
+              </Link>
+            </li>
           ))}
         </ul>
       </div>
     </MainLayout>
   )
+}
+
+Posts.getInitialProps = async (ctx) => {
+  const res = await fetch(`${baseUrl}/posts`)
+  const posts = await res.json()
+  return { posts }
 }
 
 export default Posts
