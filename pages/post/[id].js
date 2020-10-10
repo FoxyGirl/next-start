fc74b0de86800115705018b4546be36a14414351
Elena Pavlova <lenlho22@gmail.com>
2020-10-10 21:53:35 +0300

    Add getInitialProps and try getServerSideProps

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,31 +1,55 @@
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { baseUrl } from '../../constants'
 import MainLayout from '../../components/MainLayout'
 import styles from '../../styles/Posts.module.scss'
 
-const Post = ({ post }) => {
+const Post = ({ post: serverPost }) => {
   const router = useRouter()
+  const [post, setPost] = useState(serverPost)
 
-  const { title, body } = post
+  useEffect(() => {
+    const load = async () => {
+      const res = await fetch(`${baseUrl}/posts/${router.query.id}`)
+      const post = await res.json()
+      setPost(post)
+    }
+
+    if (!serverPost) {
+      load()
+    }
+  }, [])
+
+  const { title, body } = post || {}
 
   return (
     <MainLayout title={`Post: ${router.query.id}`}>
       <div className="main">
-        <h1>{title}</h1>
-        <hr />
-        <p>{body}</p>
-        <hr />
-        <Link href="/posts">
-          <a className={styles.link}>Back to all posts</a>
-        </Link>
+        {!post ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <h1>{title}</h1>
+            <hr />
+            <p>{body}</p>
+            <hr />
+            <Link href="/posts">
+              <a className={styles.link}>Back to all posts</a>
+            </Link>
+          </>
+        )}
       </div>
     </MainLayout>
   )
 }
 
-Post.getInitialProps = async (ctx) => {
-  const { id } = ctx.query
+Post.getInitialProps = async ({ query, req }) => {
+  if (!req) {
+    return { post: null }
+  }
+
+  const { id } = query
   const res = await fetch(`${baseUrl}/posts/${id}`)
   const post = await res.json()
   return { post }
--- pages/posts.js
+++ pages/posts.js
@@ -1,30 +1,62 @@
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { baseUrl } from '../constants'
 import MainLayout from '../components/MainLayout'
 
-const Posts = ({ posts }) => {
+const Posts = ({ posts: serverPosts }) => {
+  console.log(serverPosts)
+  const [posts, setPosts] = useState(serverPosts)
+
+  useEffect(() => {
+    const load = async () => {
+      const res = await fetch(`${baseUrl}/posts`)
+      const posts = await res.json()
+      setPosts(posts)
+    }
+
+    if (!serverPosts) {
+      load()
+    }
+  }, [])
+
   return (
     <MainLayout title="Posts">
       <div className="main">
-        <h1>Posts Page</h1>
-        <ul>
-          {posts.map(({ id, title }) => (
-            <li key={id}>
-              <Link href="/post/[id]" as={`/post/${id}`}>
-                <a>{title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {!posts ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <h1>Posts Page</h1>
+            <ul>
+              {posts.map(({ id, title }) => (
+                <li key={id}>
+                  <Link href="/post/[id]" as={`/post/${id}`}>
+                    <a>{title}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </MainLayout>
   )
 }
 
-Posts.getInitialProps = async (ctx) => {
+Posts.getInitialProps = async ({ req }) => {
+  if (!req) {
+    return { posts: null }
+  }
+
   const res = await fetch(`${baseUrl}/posts`)
   const posts = await res.json()
   return { posts }
 }
+
+// export const getServerSideProps = async (ctx) => {
+//   const res = await fetch(`${baseUrl}/posts`)
+//   const posts = await res.json()
+//   return { props: { posts } }
+// }
 
 export default Posts
