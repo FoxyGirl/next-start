2c24e6ef5ff243becbf12f4ba64e74ee1100014e
Elena Pavlova <lenlho22@gmail.com>
2020-10-10 22:33:42 +0300

    Add a strange progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import NextNprogress from 'nextjs-progressbar'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="#29D"
+        height={5}
+        startPosition={0.3}
+        stopDelayMs={550}
+        options={{
+          easing: 'ease',
+          speed: 500,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,20 +1,40 @@
 import Router from 'next/router'
+import { baseUrl } from '../../constants'
 import MainLayout from '../../components/MainLayout'
 
-const About = () => {
+const About = ({ about }) => {
   const handlerGoHome = () => {
     Router.push('/')
   }
 
+  console.log('about', about)
+  const { title } = about || {}
+
   return (
     <MainLayout title="About">
       <div className="main">
-        <h1>About Page</h1>
+        <h1>{title}</h1>
         <button onClick={handlerGoHome}>Go back to Home</button>
         <button onClick={() => Router.push('/posts')}>Go back to Posts</button>
       </div>
     </MainLayout>
   )
+}
+
+// About.getInitialProps = async ({ query, req }) => {
+//   if (!req) {
+//     return { about: null }
+//   }
+
+//   const res = await fetch(`${baseUrl}/about`)
+//   const about = await res.json()
+//   return { about }
+// }
+
+export const getServerSideProps = async (ctx) => {
+  const res = await fetch(`${baseUrl}/about`)
+  const about = await res.json()
+  return { props: { about } }
 }
 
 export default About
